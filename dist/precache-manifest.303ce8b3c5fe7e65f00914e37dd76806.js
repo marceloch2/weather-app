805db18dacf9dc61311ea7154f15064663b4af73
Marcelo Henrique Carneiro <marceloch2@gmail.com>
2019-04-08 13:34:43 +0200

    Master build for Heroku

---
+++ dist/precache-manifest.303ce8b3c5fe7e65f00914e37dd76806.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ceeb36224957106fe0cf",
+    "url": "/js/chunk-vendors.4d0be80a.js"
+  },
+  {
+    "revision": "ae504adcfff7c34c7d6b",
+    "url": "/js/app.b6bf18eb.js"
+  },
+  {
+    "revision": "c2ba52657194795223d82a98b67ce6e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ceeb36224957106fe0cf",
+    "url": "/css/chunk-vendors.188e04d6.css"
+  },
+  {
+    "revision": "ae504adcfff7c34c7d6b",
+    "url": "/css/app.ce7aa058.css"
+  }
+];
